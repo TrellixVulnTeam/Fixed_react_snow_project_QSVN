3d6e7b53d2cc33077927fb6804fbaadd58c88228
Kevinfd12 <kevinfd12@gmail.com>
2022-08-01 14:45:01 -0400

    handler fix

--- server/routes/handler.js
+++ server/routes/handler.js
@@ -7,21 +7,21 @@ router.get('/BackendData', (req, res) => {
             "name": 'Joe',
             "email": "Joe12@gmail.com",
             "phone": "589-8576",
-            "skill level": "amatuer",
+            "skill_level": "amatuer",
             "gender": "male"
         },
         {
             "name": 'Jolie',
             "email": "Jolie78@outlook.com",
             "phone": "457-2398",
-            "skill level": "proficient",
+            "skill_level": "proficient",
             "gender": "female"
         },
         {
             "name": 'Mark',
             "email": "Mark34@outlook.com",
             "phone": "634-9868",
-            "skill level": "expert",
+            "skill_level": "expert",
             "gender": "male"
         },
     ];
