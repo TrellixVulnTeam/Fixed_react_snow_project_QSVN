0456f9de9437c81ab1fe29ab9e3275c1407d0168
Kevinfd12 <kevinfd12@gmail.com>
2022-08-03 11:03:48 -0400

    Email system and styling

--- client/src/components/Main/components/Contact/components/Home.tsx
+++ client/src/components/Main/components/Contact/components/Home.tsx
@@ -1,68 +1,82 @@
-import React, { useState } from 'react'
+import React from 'react'
+import './Home.scss'
+import emailjs from 'emailjs-com'
 
-function Home() {
-    const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        phone: '',
-        skill_level: 'amatuer',
-        gender: '',
-    })
-    
-    const onChangeHandler = (event: any) => {
-        setFormData(() => ({
-            ...formData,
-            [event.target.name]: event.target.value
-        }))
+export const Home = () => {
+  
+    const sendEmail = (e: any) => {
+      e.preventDefault();
+  
+      emailjs.sendForm('service_sqb235x', 'template_do3t8en', e.target, 'Eanj1W0BJcs7hPaQS')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+
+      e.target.reset();
     }
-    
-    const onSubmitHandler = (event: any) => {
-        event.preventDefault()
-        console.log(formData)
-    }
+
 return (
     <>
     <h1 className="section__title">Contact Us</h1>
-    <form onSubmit={onSubmitHandler}>
+    <form onSubmit={sendEmail}>
+    
     <div className='form-group'>
-        <label htmlFor='name' className='form-label'>name</label> <br/>
-        <input className='form-control' name='name' onChange={onChangeHandler}/>
+        <label htmlFor='name' className='form-label'>Full Name</label>
+        <input className='form-control' name='name'/>
     </div>
+
     <div className='form-group'>
-        <label htmlFor='email' className='form-label'>email</label><br/>
-        <input className='form-control' name='email' onChange={onChangeHandler}/>
+        <label htmlFor='email' className='form-label'>Email Address</label>
+        <input className='form-control' name='email'/>
     </div>
+
     <div className='form-group'>
-        <label htmlFor='phone' className='form-label'>phone</label><br/>
-        <input className='form-control' name='phone' onChange={onChangeHandler}/>
+        <label htmlFor='phone' className='form-label'>Phone</label>
+        <input className='form-control' name='phone'/>
     </div>
+
     <div className='form-group'>
-        <label htmlFor='skill_level' className='form-label'>skill level</label><br/>
-        <select className='form-select' name='skill_level' onChange={onChangeHandler}>
-            <option value='amatuer'>amatuer</option>
-            <option value='proficient'>proficient</option>
-            <option value='expert'>expert</option>
+        <label htmlFor='skill_level' className='form-label'>Skill Level</label>
+        <select className='form-select' name='skill_level'>
+            <option value='amatuer'>Amatuer</option>
+            <option value='proficient'>Proficient</option>
+            <option value='expert'>Expert</option>
         </select>
     </div>
+
     <div className='form-group'>
-        <label htmlFor='gender' className='form-label'>gender</label><br/>
+        <label htmlFor='gender' className='form-label'>Gender</label>
         <div>
             <div>
-                <input type='radio' name='gender' value='male' onChange={onChangeHandler}/>
-                <label htmlFor='male'>male</label>
+                <input type='radio' name='gender' value='male'/>
+                <label htmlFor='male' className='gender-option'>Male</label>
             </div>
             <div>
-                <input type='radio' name='gender' value='female' onChange={onChangeHandler}/>
-                <label htmlFor='female'>female</label>
+                <input type='radio' name='gender' value='female'/>
+                <label htmlFor='female' className='gender-option'>Female</label>
             </div>            <div>
-                <input type='radio' name='gender' value='other' onChange={onChangeHandler}/>
-                <label htmlFor='other'>other</label>
+                <input type='radio' name='gender' value='other'/>
+                <label htmlFor='other' className='gender-option'>Other</label>
             </div>
         </div>
     </div>
+
+    <div className='form-group'>
+        <label htmlFor='subject' className='form-label'>Subject</label>
+        <input className='form-control' name='subject'/>
+    </div>
+
+    <div className='form-group'>
+        <label htmlFor='description' className='form-label'>Description</label>
+        <textarea className='form-text' name='description'/>
+    </div>
+
     <div>
         <button className='btn' type='submit'>Submit here</button>
     </div>
+
     </form>
     </>
 )
